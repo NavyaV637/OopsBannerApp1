0aecb3a5b63122e8fa6e3d3eda8540fd7d67a4c7
NavyaV637 <navyavraman@gmail.com>
2026-03-11 13:50:59 +0530

    Initial commit'

---
+++ OopsBannerApp1.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp - Use Case 1
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Navya
+ * @version 1.0
+ */
+
+class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
