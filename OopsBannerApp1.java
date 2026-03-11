c4189fefb88f7adfbb9dcaae51e8ca6f14734c57
NavyaV637 <navyavraman@gmail.com>
2026-03-11 14:00:55 +0530

    Added UC2

--- OopsBannerApp1.java
+++ OopsBannerApp1.java
@@ -1,14 +1,11 @@
-/**
- * OOPSBannerApp - Use Case 1
- * Prints the literal text "OOPS" to the console.
- *
- * @author Navya
- * @version 1.0
- */
-
-class OOPSBannerApp {
-
+class UC2_OOPS_Banner {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * * * * *  ***** ");
+        System.out.println("*     * *     * *             *");
+        System.out.println("*     * *     * *       *     *");
+        System.out.println(" *****   *****  *        ***** ");
     }
 }
