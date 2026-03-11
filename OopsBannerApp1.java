a61cbc204b2d5c01a78158d9db7aaa86fb5b1d21
NavyaV637 <navyavraman@gmail.com>
2026-03-11 14:18:43 +0530

    Added UC5

--- OopsBannerApp1.java
+++ OopsBannerApp1.java
@@ -1,13 +1,14 @@
-class UC4_OOPS_Banner_ArrayLoop {
+class UC5_OOPS_Banner_ArrayInitialization {
     public static void main(String[] args) {
-        String[] banner = new String[7];
-        banner[0] = String.join(" ", "*****", "*****", "*****", "*****");
-        banner[1] = String.join(" ", "*   *", "*   *", "*   *", "*   *");
-        banner[2] = String.join(" ", "*   *", "*   *", "*   *", "*");
-        banner[3] = String.join(" ", "*   *", "*   *", "* * *", "*****");
-        banner[4] = String.join(" ", "*   *", "*   *", "*    ", "    *");
-        banner[5] = String.join(" ", "*   *", "*   *", "*    ", "*   *");
-        banner[6] = String.join(" ", "*****", "*****", "*    ", "*****");
+        String[] banner = {
+            String.join(" ", "*****", "*****", "*****", "*****"),
+            String.join(" ", "*   *", "*   *", "*   *", "*   *"),
+            String.join(" ", "*   *", "*   *", "*   *", "*"),
+            String.join(" ", "*   *", "*   *", "* * *", "*****"),
+            String.join(" ", "*   *", "*   *", "*    ", "    *"),
+            String.join(" ", "*   *", "*   *", "*    ", "*   *"),
+            String.join(" ", "*****", "*****", "*    ", "*****")
+        };
         for (String line : banner) {
             System.out.println(line);
         }
