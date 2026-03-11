2a4097c8da67c4be018b46dec5fbb6adfe946242
NavyaV637 <navyavraman@gmail.com>
2026-03-11 21:51:46 +0530

    Added UC6

--- OopsBannerApp1.java
+++ OopsBannerApp1.java
@@ -1,16 +1,44 @@
-class UC5_OOPS_Banner_ArrayInitialization {
+class UC6_OOPS_Banner_StaticFunctions {
     public static void main(String[] args) {
-        String[] banner = {
-            String.join(" ", "*****", "*****", "*****", "*****"),
-            String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-            String.join(" ", "*   *", "*   *", "*   *", "*"),
-            String.join(" ", "*   *", "*   *", "* * *", "*****"),
-            String.join(" ", "*   *", "*   *", "*    ", "    *"),
-            String.join(" ", "*   *", "*   *", "*    ", "*   *"),
-            String.join(" ", "*****", "*****", "*    ", "*****")
-        };
-        for (String line : banner) {
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
+        for (int i = 0; i < 7; i++) {
+            String line = String.join(" ", O[i], O[i], P[i], S[i]);
             System.out.println(line);
         }
+    }
+    public static String[] buildO() {
+        return new String[]{
+            "*****",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*****"
+        };
+    }
+    public static String[] buildP() {
+        return new String[]{
+            "*****",
+            "*   *",
+            "*   *",
+            "*****",
+            "*",
+            "*",
+            "*"
+        };
+    }
+    public static String[] buildS() {
+        return new String[]{
+            "*****",
+            "*",
+            "*",
+            "*****",
+            "    *",
+            "    *",
+            "*****"
+        };
     }
 }
